55ec983b0de1d8195e8491720299cd476aba4698
Hunter Lacefield <lace1010@gmail.com>
2021-02-27 19:26:38 +0800

    Installed and added multer to get infor from file that is being uploaded in html. Then respond with an object giving the name type and size of the file uploaded

--- server.js
+++ server.js
@@ -1,20 +1,34 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
-var app = express();
+// Got these two lines from https://expressjs.com/en/resources/middleware/multer.html
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+
+const app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-
-
+// Bc we are only uploading one file we use upload.single("") string inside that we are uploading must be same as name in html
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  if (req.file) {
+    console.log(req.file);
+    let responseObject = {
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size,
+    };
+    res.json(responseObject);
+  } else return res.json({ error: "No file was selected for upload" });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
